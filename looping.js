5e64ff44a015299dd1d6da70e439245c99020184
deller26 <deller26@gmail.com>
2015-03-27 12:32:01 -0700

    Update looping.js

--- looping.js
+++ looping.js
@@ -1,31 +1,22 @@
-function nextInfoSlide() {			
-	if($('.info-slide.active').next('.info-slide').length > 0) {
-	// One-liner slide change if there's more left
-	$('.info-slide.active').removeClass('active').next('.info-slide').addClass('active').find('.progress-bar').ICM_ProgressBar('start');
-
-	var $slides = $('.info-slide');
-	setTimeout(function() {
-		var totalBarLength = Math.floor((100 / $slides.length) * ($slides.index($('.info-slide.active')) + 1));
-		$('#total-progress .progress-bar').ICM_ProgressBar('animateTo', totalBarLength, infoSlideProgressBarTimer);
-	}, 350);
-	} else {
-		nextOverlayStep();
-	}
-}
-
-
-/*****************
-This works below
-*****************/
-
-// make first child active!
-
+// does not loop
 function checkThis() {
-if ($('.socials.active').next('.socials').length > 0) {
-	setTimeout(function() {
-		$('.socials.active').removeClass('active').next('.socials').addClass('active');
-		checkThis();
-	}, 2000);
-}
+  if ($('.socials.active').next('.socials').length > 0) {
+    setTimeout(function() {
+      $('.socials.active').removeClass('active').next('.socials').addClass('active');
+      checkThis();
+    }, 1000);
+  }
 }
 checkThis();
+
+// loops
+function checkThat() {
+  if ($('ul li:first-child').next('li').length > 0) {
+    setTimeout(function() {
+      $('ul li.active').removeClass('active').next('li').addClass('active');
+      checkThat();
+      $('ul li:first-child').appendTo($('ul'));
+    }, 1000);
+  }
+}
+checkThat();
